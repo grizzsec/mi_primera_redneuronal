b464b3cc4c24cd7e94e40b33248a8d29c9a1d81e
Christian González <107643906+chretiengonzalez@users.noreply.github.com>
2022-06-29 12:17:38 +0200

    Add files via upload

---
+++ mi_primera_red_neuronal.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+#modelo = tf.keras.Sequential([capa])
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1, oculta2, salida])
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+
+print("Comenzando entrenamiento...")
+historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
+print("Modelo entrenado!")
+
+import matplotlib.pyplot as plt
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+
+print("Hagamos una predicción!")
+resultado = modelo.predict([100.0])
+print("El resultado es " + str(resultado) + " fahrenheit!")
+
+print("Variables internas del modelo")
+#print(capa.get_weights())
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
